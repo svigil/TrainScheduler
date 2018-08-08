0c4b0af1247e21e8d3348fdcae38659ac9c34af7
Spencer Vigil <spencervigil27@gmail.com>
2018-08-08 17:32:11 -0500

    initial commit

---
+++ assets/java/app.js
@@ -0,0 +1,7 @@
+// Create html placeholders for what needs to be displayed
+// Point to correct file for js
+// Point ot correct jQuery
+// Start with creating global vars
+// Create functions needed
+//  Within functions create local vars
+//  Display what needs to happen within the function
